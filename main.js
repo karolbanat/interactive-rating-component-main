26a520ef63a8127b81911e57686830ab34add159
Karol Banat <karol.banat@gmail.com>
2022-04-19 13:25:08 +0200

    Challenge completed

---
+++ main.js
@@ -0,0 +1,30 @@
+const rateBtns = document.querySelectorAll('.rating__rate-btn');
+const submitBtn = document.querySelector('.rating__submit-btn');
+const ratingCard = document.querySelector('.rating');
+const thankYouCard = document.querySelector('.thank-you');
+const endRating = document.querySelector('.thank-you__end-rating');
+
+rateBtns.forEach((btn) => {
+	btn.addEventListener('click', (e) => {
+		// removes selected class from all rating buttons
+		rateBtns.forEach((button) => {
+			button.classList.remove('rating__rate-btn--checked');
+			button.ariaSelected = 'false';
+		});
+		// and adds selected class to clicked button
+		e.target.classList.add('rating__rate-btn--checked');
+		e.target.ariaSelected = 'true';
+	});
+});
+
+submitBtn.addEventListener('click', (e) => {
+	// prevents reloading the page on submit
+	e.preventDefault();
+	// checkes if any rating is selected
+	if (document.querySelector('.rating__rate-btn--checked')) {
+		const selectedRating = document.querySelector('.rating__rate-btn--checked').value;
+		ratingCard.classList.add('card--hidden');
+		thankYouCard.classList.remove('card--hidden');
+		endRating.innerText = selectedRating;
+	}
+});
